8d8040c787a6f11ad5081963b6642a026defc37f
galaa3 <alessandro.galante@mail.polimi.it>
2026-07-31 11:37:32 +0200

    CorsFilter added

--- backend/src/main/java/org/tripm/filters/CorsFilter.java
+++ backend/src/main/java/org/tripm/filters/CorsFilter.java
@@ -1,4 +1,41 @@
-package PACKAGE_NAME;
+package org.tripm.filters;
 
-public class CorsFIlter {
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * this is a requests filter to avoid the CORS browser system
+ */
+@WebFilter("/*")
+public class CorsFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+
+        String frontendUrl = System.getenv("FRONTEND_URL");
+
+        // for local developing
+        if (frontendUrl == null || frontendUrl.trim().isEmpty()) {
+            frontendUrl = "http://localhost:5173";
+        }
+
+        // setting response
+        res.setHeader("Access-Control-Allow-Origin", frontendUrl);
+        res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+        if ("OPTIONS".equalsIgnoreCase(req.getMethod())) {
+            res.setStatus(HttpServletResponse.SC_OK);
+            return;
+        }
+
+        chain.doFilter(request, response);
+    }
 }
