65042478a9b395503fe1d3ed73d161894d7687c6
galaa3 <alessandro.galante@mail.polimi.it>
2026-08-01 20:52:14 +0200

    SignIn

--- backend/src/main/java/org/tripm/controllers/SignIn.java
+++ backend/src/main/java/org/tripm/controllers/SignIn.java
@@ -12,8 +12,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/api/signup")
-public class SignUp extends HttpServlet {
+@WebServlet("/api/signin")
+public class SignIn extends HttpServlet {
     private final UserDAO userDao = new UserDAO();
     private final Gson gson = new Gson();
 
@@ -25,27 +25,32 @@ public class SignUp extends HttpServlet {
 
         try{
             BufferedReader reader = req.getReader();
-            User newUser = gson.fromJson(reader, User.class);
+            User possibleUser = gson.fromJson(reader, User.class);
 
-            if(newUser.getUsername() == null || newUser.getEmail() == null || newUser.getPassword() == null){
-                resp.setStatus(HttpServletResponse.SC_BAD_REQUEST); //400
+            if (possibleUser == null || possibleUser.getUsername() == null || possibleUser.getPassword() == null) {
+                resp.setStatus(HttpServletResponse.SC_BAD_REQUEST); // 400 Bad Request
                 out.print("{\"success\": false, \"message\": \"All fields are mandatory.\"}");
                 return;
             }
 
-            boolean isRegistered = userDao.registerUser(newUser);
-            if(isRegistered){
-                resp.setStatus(HttpServletResponse.SC_CREATED); // 201
-                out.print("{\"success\": true, \"message\": \"Registration successfully completed.\"}");
-            }else {
-                resp.setStatus(HttpServletResponse.SC_CONFLICT); // 409
-                out.print("{\"success\": false, \"message\": \"Username or email already in use.\"}");
+            User loggedUser = userDao.checkCredentials(possibleUser.getUsername(), possibleUser.getPassword());
+
+            if (loggedUser != null) {
+                resp.setStatus(HttpServletResponse.SC_OK); // 200
+
+                loggedUser.setPassword(null);
+                String userJson = gson.toJson(loggedUser);
+                out.print("{\"success\": true, \"user\": " + userJson + "}");
+            } else {
+                // Login failed
+                resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 401
+                out.print("{\"success\": false, \"message\": \"invalid credentials.\"}");
             }
+
         } catch (Exception e) {
-            // handling generic server errors
+            System.err.println("SignIn error: " + e.getMessage());
             resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR); // 500
             out.print("{\"success\": false, \"message\": \"Internal server error.\"}");
         }
-
     }
 }
