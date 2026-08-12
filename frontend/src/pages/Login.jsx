fff9fa34aa74d80f5f425833aa0d0d786cf72036
galaa3 <alessandro.galante@mail.polimi.it>
2026-08-12 20:10:47 +0200

    login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,19 +1,77 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [errorMsg, setErrorMsg] = useState('');
+
+    const navigate = useNavigate();
+
+    const handleLogin = async(e) => {
+        e.preventDefault();
+        setErrorMsg('');
+
+        try{
+            const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/signin`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    username: username,
+                    password: password
+                })
+            });
+
+            const data = await response.json();
+
+            if(data.success){
+                // testing
+                console.log("User from DB: ", data.user);
+
+                navigate('/');
+            }else{
+                setErrorMsg(data.message);
+            }
+        }catch (error){
+            setErrorMsg("Connection to server failed.");
+        }
+    }
+
     return (
         <div className="form-container">
             <h2>Login</h2>
 
-            <form className="auth-form">
-                <input type="text" placeholder="Username" className="form-input" />
-                <input type="password" placeholder="Password" className="form-input" />
-                <button type="submit" className="form-submit-btn">Entra</button>
+            {errorMsg && (
+                <div className="errorMsg">
+                    {errorMsg}
+                </div>
+            )}
+
+            <form className="auth-form" onSubmit={handleLogin}>
+                <input
+                    type="text"
+                    placeholder="Username"
+                    className="form-input"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    className="form-input"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+                <button type="submit" className="form-submit-btn">Login</button>
             </form>
 
             <div className="form-footer">
                 Non hai ancora un account?{' '}
-                <Link to="/signup" className="form-link">Registrati qui</Link>
+                <Link to="/signup" className="form-link">Sign up here</Link>
             </div>
         </div>
     );
